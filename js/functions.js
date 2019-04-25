4015d9c8be81b534309c3826f22dcf8724a88646
Elio Osés <eliooses@netfilmx.com>
2019-04-25 02:04:58 -0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -21,7 +21,7 @@ function catalogue(hash, imdb, magnet, title, rating, poster, genre, background,
 	html+='<div id="movie-box-'+'" class="movie-box movie-box-'+genre+' col-lg-2 col-md-3 col-sm-6 col-xs-12" style="position:relative;float:left;">';
 	html+='<div style="width: 100%; height: 330px; position: absolute;left: 0px; top: 0px; -webkit-filter: blur(3px); -moz-filter: blur(3px); -o-filter: blur(3px); -ms-filter: blur(3px); filter: blur(3px); background-image:url('+background+'); background-position: center; background-size: cover;  background-repeat: no-repeat; -webkit-box-shadow: inset 0px 0px 30px 30px rgba(0, 0, 0, 1); -moz-box-shadow: inset 0px 0px 30px 30px rgba(0, 0, 0, 1); box-shadow: inset 0px 0px 30px 30px rgba(0, 0, 0, 1);"></div>';
 	// html+='<a href="'+magnet+'" target="_blank">';
-	html+='<img id="img-movie-box-'+hash+'" class="hover-luz" title="'+title+'" alt="'+title+'" src="'+poster+'" onclick="openNewWindow(1080, 720, &#39;'+magnet+'&#39;); openNewWindowExtra();" onmouseover="hoverHash(&#39;'+imdb+'&#39;)" onmouseout="outHash(&#39;'+imdb+'&#39;)"/>';
+	html+='<img id="img-movie-box-'+hash+'" class="hover-luz" title="'+title+'" alt="'+title+'" src="'+poster+'" onclick="openNewWindow(1080, 720, &#39;'+magnet+'&#39;)" onmouseover="hoverHash(&#39;'+imdb+'&#39;)" onmouseout="outHash(&#39;'+imdb+'&#39;)"/>';
 	html+='<div id="movie-rating-star-'+imdb+'" class="movie_rating_star"></div></div>';
 	// html+='</a>';
 	$('#movies').append(html);
@@ -45,12 +45,4 @@ function openNewWindow(width, height, hash) {
 	var left = (screen.availWidth / 2) - (width / 2);
 	var features = "location=1, status=1, scrollbars=1, width=" + width + ", height=" + height + ", top=" + top + ", left=" + left;
 	var customWindow = window.open(hash, "kad", features);
-}
-function openNewWindowExtra() {
-	var width = 1080;
-	var height = 360;
-	var top = (screen.availHeight / 2) + (width / 2);
-	var left = (screen.availWidth / 2) + (height / 2);
-	var features = "location=1, status=1, scrollbars=1, width=" + width + ", height=" + height + ", top=" + top + ", left=" + left;
-	var customWindow2 = window.open("https://codepen.io/eliooses/pen/vMQpNO", "las", features);
 }
