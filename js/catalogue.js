5d3f8d7b2215f993d975eaa73dce557e1457f889
Elio Osés <eliooses@netfilmx.com>
2019-09-30 16:56:33 -0300

    Update catalogue.js

--- js/catalogue.js
+++ js/catalogue.js
@@ -15,7 +15,7 @@ $(function () {
 	var hash, imdb, magnet, title, rating, poster, genre, background;	
 	// DATASET
 
-	var proxy = "corsio";
+	var proxy = "none";
 
 	switch (proxy) {
 		case "crossorigin":					proxy = "http://crossorigin.me/"; break;
